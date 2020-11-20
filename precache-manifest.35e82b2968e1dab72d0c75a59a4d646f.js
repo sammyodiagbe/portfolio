232cdb84752a83a3aad551a963e06c4d5f585d4b
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2020-11-20 10:04:17 +0100

    Updates

---
+++ precache-manifest.35e82b2968e1dab72d0c75a59a4d646f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88911157922e79844f573892dfd8edba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "59dea829819c3ce09bc8",
+    "url": "/portfolio/static/css/main.0d87c257.chunk.css"
+  },
+  {
+    "revision": "26fa4eff25799c12fe6d",
+    "url": "/portfolio/static/js/2.62a47d6a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.62a47d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59dea829819c3ce09bc8",
+    "url": "/portfolio/static/js/main.9af3005a.chunk.js"
+  },
+  {
+    "revision": "2efea831254d656aa647",
+    "url": "/portfolio/static/js/runtime-main.1d4f1136.js"
+  },
+  {
+    "revision": "ea5b5e05c0a0d3c822b6d3b9d6e150bf",
+    "url": "/portfolio/static/media/author.ea5b5e05.webp"
+  },
+  {
+    "revision": "9150812fbf17b9d7e990630713e65296",
+    "url": "/portfolio/static/media/bak.9150812f.webp"
+  },
+  {
+    "revision": "94b656babc2faa8419eb61c5697580e7",
+    "url": "/portfolio/static/media/featured_1.94b656ba.webp"
+  },
+  {
+    "revision": "2cb71d998f98f6f78f0f05c35729bc2e",
+    "url": "/portfolio/static/media/featured_2.2cb71d99.webp"
+  },
+  {
+    "revision": "d1496dae994f06500d5e10295f64def7",
+    "url": "/portfolio/static/media/featured_3.d1496dae.webp"
+  },
+  {
+    "revision": "8e0c65c6edbca826b2f20aaac6a65f50",
+    "url": "/portfolio/static/media/latest_1.8e0c65c6.webp"
+  },
+  {
+    "revision": "1b3c4f2fda197e20030414c87c94562e",
+    "url": "/portfolio/static/media/latest_1x.1b3c4f2f.webp"
+  },
+  {
+    "revision": "d9fccdd916c526cd93708b71ed0faab7",
+    "url": "/portfolio/static/media/latest_2.d9fccdd9.webp"
+  },
+  {
+    "revision": "1428c530c0f9514fb5567cad77baf11f",
+    "url": "/portfolio/static/media/latest_2x.1428c530.webp"
+  },
+  {
+    "revision": "313fbec4b7be1330867056895e397ffb",
+    "url": "/portfolio/static/media/latest_3.313fbec4.webp"
+  },
+  {
+    "revision": "12d3e5681d590c1921a921da27e52f24",
+    "url": "/portfolio/static/media/latest_3x.12d3e568.webp"
+  }
+]);
